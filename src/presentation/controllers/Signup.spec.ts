d87114e7322212d8e08343f25cd070a8e4106f84
Murilo Vanucci <murilo_vanucci@hotmail.com>
2021-01-06 16:50:32 -0300

    test: ensure SignUpController return 400 if no password confirmation is provided

--- src/presentation/controllers/Signup.spec.ts
+++ src/presentation/controllers/Signup.spec.ts
@@ -43,4 +43,18 @@ describe('Signup Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamsErrors('password'))
   })
+
+  test('Should return 400, if no password confirm is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamsErrors('passwordConfirm'))
+  })
 })
