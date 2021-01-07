62c61f50f2745687e7e8f6fecb9852ddc3118da6
Murilo Vanucci <murilo_vanucci@hotmail.com>
2021-01-07 12:26:00 -0300

    test: ensure SingUpController calls EmailValidator with correct email

--- src/presentation/controllers/Signup.spec.ts
+++ src/presentation/controllers/Signup.spec.ts
@@ -95,4 +95,21 @@ describe('Signup Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new InvalidParamError('email'))
   })
+
+  test('Should call EmailValidator with correct email', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirm: 'any_password'
+      }
+    }
+    sut.handle(httpRequest)
+
+    expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
 })
