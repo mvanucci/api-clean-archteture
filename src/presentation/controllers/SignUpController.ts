55424582653997a331af0f03af27829affb4af76
Murilo Vanucci <murilo_vanucci@hotmail.com>
2021-01-08 09:19:43 -0300

    feat: ensure SignUpController returns 400 if password confirmation fails

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -16,6 +16,10 @@ export class SignUpController implements Controller {
           return badRequest(new MissingParamsErrors(field))
         }
       }
+      if (httpRequest.body.password !== httpRequest.body.passwordConfirm) {
+        return badRequest(new InvalidParamError('passwordConfirm'))
+      }
+
       const isValid = this.emailValidator.isValid(httpRequest.body.email)
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
