1787bc17c906e7b7726979d7a6b1998006ad07e2
Murilo Vanucci <murilo_vanucci@hotmail.com>
2021-01-25 17:24:58 -0300

    feat: ensure requests return json as default content type

---
+++ src/main/middlewares/content-type.test.ts
@@ -0,0 +1,24 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Content-type Middleware', () => {
+  test('should return default content-type as json',async () => {
+    app.get('/test_content_type', (req, res) => {
+      res.send()
+    })
+    await request(app)
+      .get('/test_content_type')
+      .send()
+      .expect('content-type', /json/)
+  })
+  test('should return xml content-type an forced',async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+    await request(app)
+      .get('/test_content_type_xml')
+      .send()
+      .expect('content-type', /xml/)
+  })
+})
--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -1,7 +1,7 @@
 import request from 'supertest'
 import app from '../config/app'
 
-describe('Body Parser Middleware', () => {
+describe('CORS Middleware', () => {
   test('should enable cors',async () => {
     app.get('/test_cors', (req, res) => {
       res.send(req.body)
