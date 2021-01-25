101d023b20e5261c00975df08201aecb8b061c9c
Murilo Vanucci <murilo_vanucci@hotmail.com>
2021-01-25 16:04:24 -0300

    feat: ensure mongodb is connection is down

--- src/infra/db/mongodb/helpers/mongodb-helper.ts
+++ src/infra/db/mongodb/helpers/mongodb-helper.ts
@@ -16,7 +16,7 @@ export const MongoHelper = {
     this.client = null
   },
   async getCollection (name: string): Promise<Collection> {
-    if (!this.client.isConnected()) {
+    if (!this.client?.isConnected()) {
       await this.connect(this.uri)
     }
 
