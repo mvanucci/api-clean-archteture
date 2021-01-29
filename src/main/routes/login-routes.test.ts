691b80d88cf8dd406eb6a6a20e22ad0467a5fe02
Murilo Vanucci <murilo_vanucci@hotmail.com>
2021-01-28 22:10:00 -0300

    refactor: rename signup Routes with login-routes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongodb-helper'
 import app from '../config/app'
 
-describe('SignUp Routes', () => {
+describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
@@ -14,16 +14,17 @@ describe('SignUp Routes', () => {
     const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
-
-  test('should return an account on success',async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'Murilo',
-        email: 'murilo_vanucci@hotmail.com',
-        password: '123',
-        passwordConfirm: '123'
-      })
-      .expect(200)
+  describe('POST /sinup', () => {
+    test('should return 200 SignUp',async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Murilo',
+          email: 'murilo_vanucci@hotmail.com',
+          password: '123',
+          passwordConfirm: '123'
+        })
+        .expect(200)
+    })
   })
 })
