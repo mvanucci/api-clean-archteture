3d515aa8b6d9a3937ac41903000db994bd075aa7
Murilo Vanucci <murilo_vanucci@hotmail.com>
2021-01-28 22:45:59 -0300

    test: login route returns 401 with invalid credentias

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -50,5 +50,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'murilo_vanucci@hotmail.com',
+          password: '123'
+        })
+        .expect(401)
+    })
   })
 })
