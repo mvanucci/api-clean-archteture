18114c5a978e6edfe238dfaf43f710f558ebc2b0
Murilo Vanucci <murilo_vanucci@hotmail.com>
2021-02-03 10:59:49 -0300

    feat: ensure AddSurveyController returns 204 on success

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,5 +1,5 @@
 import { AddSurvey } from '../../../../domain/usecases/AddSurvey'
-import { badRequest, serverError } from './../../../helpers/http/HttpHelpers'
+import { badRequest, noContent, serverError } from './../../../helpers/http/HttpHelpers'
 import { Controller, HttpRequest, HttpResponse, Validation } from './add-survey-controller-protocols'
 
 export class AddSurveyController implements Controller {
@@ -19,7 +19,7 @@ export class AddSurveyController implements Controller {
         question,
         answers
       })
-      return null
+      return noContent()
     } catch (error) {
       return serverError(error)
     }
--- src/presentation/helpers/http/HttpHelpers.ts
+++ src/presentation/helpers/http/HttpHelpers.ts
@@ -25,3 +25,8 @@ export const Ok = (data: any): HttpResponse => ({
   statusCode: 200,
   body: data
 })
+
+export const noContent = (): HttpResponse => ({
+  statusCode: 204,
+  body: null
+})
