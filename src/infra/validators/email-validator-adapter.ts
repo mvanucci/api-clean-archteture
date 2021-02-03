11446458cf9f16462ad64588be2a413aec4866c0
Murilo Vanucci <murilo_vanucci@hotmail.com>
2021-02-03 14:24:53 -0300

    fix: fix the import file

--- src/infra/validators/email-validator-adapter.ts
+++ src/infra/validators/email-validator-adapter.ts
@@ -1,4 +1,5 @@
-import { EmailValidator } from '../../../validations/protocols/EmailValidator'
+
+import { EmailValidator } from '../../validations/protocols/EmailValidator'
 import validator from 'validator'
 
 export class EmailValidatorAdapter implements EmailValidator {
