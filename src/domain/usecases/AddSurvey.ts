6968ffb32819e98afa5fa44cfa5ec56007ff964c
Murilo Vanucci <murilo_vanucci@hotmail.com>
2021-02-03 10:31:15 -0300

    feat: ensure AddSurveyController calls AddSurveyUseCase with correct values

---
+++ src/domain/usecases/AddSurvey.ts
@@ -0,0 +1,14 @@
+export interface AddSurveyModel {
+  question: string
+  answers: SurveyAnswers[]
+
+}
+
+export interface SurveyAnswers {
+  image: string
+  answer: string
+}
+
+export interface AddSurvey {
+  add: (account: AddSurveyModel) => Promise<void>
+}
