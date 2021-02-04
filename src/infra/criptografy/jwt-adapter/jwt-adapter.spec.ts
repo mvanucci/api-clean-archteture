b6ea553b0f3a28a367e1250a8bcc830e795a06e5
Murilo Vanucci <murilo_vanucci@hotmail.com>
2021-02-04 00:03:33 -0300

    test: ensure jwt adapter call verify with correct values

--- src/infra/criptografy/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptografy/jwt-adapter/jwt-adapter.spec.ts
@@ -4,6 +4,9 @@ import { JwtAdapter } from './jwt-adapter'
 jest.mock('jsonwebtoken', () => ({
   async sign (): Promise<string> {
     return new Promise(resolve => resolve('any_token'))
+  },
+  async verify (): Promise<string> {
+    return new Promise(resolve => resolve('any_value'))
   }
 }))
 
@@ -32,6 +35,14 @@ describe('Jwt Adapter', () => {
       jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw new Error() })
       const promise = sut.encrypt('any_id')
       await expect(promise).rejects.toThrow()
+    })
+  })
+  describe('verify ()', () => {
+    test('should call verify with corrent values', async () => {
+      const sut = makeSut()
+      const verifySpy = jest.spyOn(jwt, 'verify')
+      await sut.decrypt('any_token')
+      expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret')
     })
   })
 })
