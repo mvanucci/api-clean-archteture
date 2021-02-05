e11d04957abe4cff19403542018381aeb9465fc5
Murilo Vanucci <murilo_vanucci@hotmail.com>
2021-02-05 11:29:55 -0300

    test: ensure load surveys route returns 200 if a valid token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -75,5 +75,34 @@ describe('Survey Routes', () => {
         .get('/api/surveys')
         .expect(403)
     })
+
+    test('should return 200 on add Survey with valid accessToken', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'Murilo',
+        email: 'murilo_vanucci@hotmail.com',
+        password: '123'
+      })
+      const id = res.ops[0]._id
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({ _id: id }, {
+        $set: {
+          accessToken
+        }
+      })
+
+      await surveyCollection.insertMany([{
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_anwser'
+        }],
+        date: new Date()
+      }])
+
+      await request(app)
+        .get('/api/surveys')
+        .set('x-access-token', accessToken)
+        .expect(200)
+    })
   })
 })
--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -3,8 +3,9 @@ import { adaptRoute } from '../adapter/express-route-adapter'
 import { Router } from 'express'
 import { adminAuth } from '../middlewares/admin-auth'
 import { auth } from '../middlewares/auth'
+import { makeLoadSurveyController } from '../factories/controllers/survey/load-surveys/load-survey-controller-factory'
 
 export default (router: Router): void => {
   router.post('/surveys', adminAuth, adaptRoute(makeAddSurveyController()))
-  router.get('/surveys', auth, adaptRoute(makeAddSurveyController()))
+  router.get('/surveys', auth, adaptRoute(makeLoadSurveyController()))
 }
