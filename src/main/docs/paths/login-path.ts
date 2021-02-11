56b97effc1a76418fa06d3098c3523350f7f94af
Murilo Vanucci <murilo_vanucci@hotmail.com>
2021-02-11 09:15:14 -0300

    docs: add login api docs

---
+++ src/main/docs/paths/login-path.ts
@@ -0,0 +1,37 @@
+export const loginPath = {
+  post: {
+    tags: ['Login'],
+    summary: 'API para autenticar usuário',
+    requestBody: {
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/schemas/loginParams'
+          }
+        }
+      }
+    },
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/account'
+            }
+          }
+        }
+      },
+      400: {
+        description: 'Bad Request',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/account'
+            }
+          }
+        }
+      }
+    }
+  }
+}
